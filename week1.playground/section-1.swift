49849f40b21d995aba402f26a380e281da5a9390
55011212106 <istudents@iClassrooms-iMac-24.local>
2015-01-23 14:27:17 +0700

    Week1
    week1

---
+++ week1.playground/section-1.swift
@@ -0,0 +1,49 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let a = 20
+let b = 30
+var ab = a+b
+
+let c:Int = 19.99
+let d:Double = 19.56
+let h:Float = 19.25
+
+let e = "TG"
+var f = [String]()
+f = ["aa","abd"]
+
+var sh = ["asd","asda"]
+println("asdasd \(sh.count) item.") // นับ array
+
+let on = true
+let no = false
+
+if (on){
+    println("aaaa")
+}else{
+    println("bbb")
+}
+
+f += ["bew"]
+f += ["ddd","fff","gggg"]
+
+let array: Array<Int> = [1,2,3,4]
+let ddd: Dictionary<String, Int> = ["dog":1,"cat":2]
+
+
+var arr1 = ["a","b","c","d"]
+for (var i = 0; i < arr1.count ;i++ ){
+    println(arr1[i])
+}
+
+var arr2 = ["cc","vv","bb"]
+var str = ""
+for str in arr2 {
+    println(str)
+}
+
+
+var str5 = "Hello, playground"
